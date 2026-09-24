54f9b3d22b452b842b4c2bf4d3ee583c9b06a8c3
ilkm4860-web <ilkm4860@gmail.com>
2026-09-24 11:00:27 +0300

    Refactor server.js to improve error handling

--- server.js
+++ server.js
@@ -8,14 +8,12 @@ const HOST = "0.0.0.0";
 
 app.disable("x-powered-by");
 
-// GitHub'daki ana klasörden dosyaları servis et
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Hata durumunda
 app.use((req, res) => {
     res.status(404).send("Sayfa bulunamadı.");
 });
